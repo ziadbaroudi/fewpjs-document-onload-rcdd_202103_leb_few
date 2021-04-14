5226c13683ba7848c8f5958befc0f540af60caa0
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-14 01:41:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  document.getElementById("text").innerText = "This is really cool!"
 });
